8d6f2c8c376f59bf2226abf51c45c3a7a3a34969
JLisarte <juliano.lisarte@gmail.com>
2023-12-22 08:47:15 -0300

    Fix file paths in router.add() function

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,9 @@ import { Router } from './router.js'
 
 const router = new Router()
 
-  router.add("/", "../src/pages/home.html")
-  router.add("/universe", "../src/pages/universe.html")
-  router.add("/exploration", "../src/pages/exploration.html")
+  router.add("/", "./src/pages/home.html")
+  router.add("/universe", "./src/pages/universe.html")
+  router.add("/exploration", "./src/pages/exploration.html")
   router.add(404, "./src/pages/404.html")
 
   router.handle()
